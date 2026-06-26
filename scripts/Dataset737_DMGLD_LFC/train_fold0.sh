e258a45c3496443a2ea453eb0886f867cb2c7641
alexveronese <302474@studenti.unimore.it>
2026-06-26 15:05:55 +0200

    fix mem requested

--- scripts/Dataset737_DMGLD_LFC/train_fold0.sh
+++ scripts/Dataset737_DMGLD_LFC/train_fold0.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=70G
+#SBATCH --mem=48G
 #SBATCH --gres=gpu:1
 #SBATCH --account=tesi_averonese
 #SBATCH --partition=all_usr_prod
