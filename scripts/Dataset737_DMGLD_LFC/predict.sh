82a1c1a2c852018c79944af2c888fdbdcc7cb0c4
alexveronese <302474@studenti.unimore.it>
2026-06-27 15:23:25 +0200

    add predict and evaluation for LFC dataset

---
+++ scripts/Dataset737_DMGLD_LFC/predict.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=nnLandmark
+#SBATCH --output=/work/tesi_averonese/nnLandmark/logs/MML_predict%j.out
+#SBATCH --error=/work/tesi_averonese/nnLandmark/logs/MML_predict%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=70G
+#SBATCH --gres=gpu:1
+#SBATCH --account=tesi_averonese
+#SBATCH --partition=all_usr_prod
+#SBATCH --time=24:00:00
+
+module unload python/3.11.11-gcc-11.4.0 
+module load python/3.10.16-gcc-11.4.0
+
+source /homes/averonese/nnLandmark/nnlandmark-venv/bin/activate
+
+export nnLM_results=/work/grana_maxillo/averonese_STS2026/nnLM/nnLM_results
+export nnLM_preprocessed=/work/grana_maxillo/averonese_STS2026/nnLM/nnLM_preprocessed
+export nnLM_raw=/work/grana_maxillo/averonese_STS2026/nnLM/nnLM_raw
+
+DATASET_DIR=/work/grana_maxillo/averonese_STS2026/nnLM/nnLM_raw/Dataset737_DMGLD_LFC/imagesTs
+DATASET_ID=737
+OUTPUT_DIR=/work/grana_maxillo/averonese_STS2026/nnLM/predictions/Dataset737_DMGLD_LFC/
+
+cd /homes/averonese/nnLandmark/
+
+nnLM_predict \
+    -i ${DATASET_DIR} \
+    -o ${OUTPUT_DIR} \
+    -d ${DATASET_ID} \
+    -c 3d_fullres \
+    -p nnUNetResEncUNetMPlans
+
+echo "Training completed successfully!"
